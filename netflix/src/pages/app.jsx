3c72c180473446f6610c105b88030e232cdd8034
Mukhesh-Sompalli <mukheshsompalli02@gmail.com>
2024-12-26 13:18:10 +0530

    add

--- netflix/src/pages/app.jsx
+++ netflix/src/pages/app.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
-import Home from './home/home'
-const App = () => {
+// import React from 'react'
+// import Home from './home/home'
+// import {Routes,Route} from 'react-router-dom'
+// import Login from '../pages/login/login'
+// const App = () => {
+//   return (
+//     <div>
+//       <Routes>
+//         <Route path='/' element={<Home/>}/>
+//         <Route path='/login' element={<Login/>}/>
+//       </Routes>
+     
+//     </div>
+//   )
+// }
+
+// export default App;
+
+
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Home from './home/home';
+import Login from '../pages/login/login';
+
+function App() {
   return (
-    <div>
-     <Home/>
-    </div>
-  )
+    <Router> {/* Wrapping everything inside Router */}
+      <Routes> {/* Define your routes */}
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={< Login/>} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- netflix/src/pages/login/login.jsx
+++ netflix/src/pages/login/login.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import './login.css'
+import logo from '../../assets1/logo.png'
+
 const Login = () => {
   return (
     <div className='login'>
-      
+      <img src={logo} className='login-logo' alt="Netflix Logo" />
+      <div className="login_form">
+        <h1>Sign Up</h1>
+        <form>
+          <input type='text' placeholder='Your name' />
+          <input type='email' placeholder='Email' />
+          <input type='password' placeholder='Password' />
+          <button>Sign Up</button>
+          <div className="for_help">
+            <div className="remember">
+              <input type="checkbox" id="remember" />
+              <label htmlFor='remember'>Remember Me</label>
+            </div>
+            <p>Need Help?</p>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
 
-export default Login
+export default Login;
